479611911b2a8d8a8c84eb375b3aa831ae96be0d
Florian Siegel <florian1siegel@googlemail.com>
2024-10-24 22:42:32 +0200

    chore: setup project configuration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,38 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Florian Siegel',
-  authorAddress: 'florian1siegel@googlemail.com',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.5.0',
-  name: 'aws-slack-cost-notification-bot',
+  author: "Florian Siegel",
+  authorAddress: "florian1siegel@googlemail.com",
+  cdkVersion: "2.164.0",
+  defaultReleaseBranch: "main",
+  jsiiVersion: "~5.5.0",
+  name: "aws-slack-cost-notification-bot",
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/florian1siegel/aws-slack-cost-notification-bot.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  repositoryUrl:
+    "https://github.com/florian1siegel/aws-slack-cost-notification-bot.git",
+  prettier: true,
+  vscode: true,
+  deps: [
+    "@aws-lambda-powertools/logger",
+  ] /* Runtime dependencies of this module. */,
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: [
+    "@types/aws-lambda",
+    "aws-lambda",
+  ] /* Build dependencies for this module. */,
   // packageName: undefined,  /* The "name" in package.json. */
+});
+
+project.vscode?.extensions.addRecommendations("esbenp.prettier-vscode");
+project.vscode?.settings.addSettings(
+  {
+    "editor.formatOnSave": true,
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+  },
+  ["typescript", "markdown", "json"],
+);
+
+project.eslint?.addRules({
+  "sort-imports": ["off"],
+  "import/order": "error",
 });
 project.synth();
